e0e0a76bf14d1824effa97081e953b93589e9ba0
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-05-11 13:44:07 -0600

    added to index

--- app/src/index.js
+++ app/src/index.js
@@ -5,15 +5,20 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
+import {Other} from "./pages/Other";
+
 
 const Routing = () => (
 	<>
+		<h2>Navbar</h2>
 		<BrowserRouter>
 			<Switch>
+				<Route exact path = "/Other" component = {Other}/>
 				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
+		<h3>Footer</h3>
 	</>
 );
 ReactDOM.render(<Routing/>, document.querySelector('#root'));
